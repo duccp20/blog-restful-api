d085e924b3b49ad04e9f2bbda6a61301c5c3edde
phuoc <duccp20@gmail.com>
2023-08-13 16:12:58 +0700

    ok

--- src/main/java/com/example/blogapprestapi/test/ok.java
+++ src/main/java/com/example/blogapprestapi/test/ok.java
@@ -2,6 +2,6 @@ package com.example.blogapprestapi.test;
 
 public class ok {
     public static void main(String[] args) {
-        System.out.println("OK456");
+        System.out.println("OK ver 1");
     }
 }
